1a927fd421c0620858c70f163fb54d244b5995e7
pllearns <thenewphilliplorenzo@outlook.com>
2020-05-28 13:06:48 -0700

    update from tslint to eslint-typescript

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-base'
+  ],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  rules: {
+    singleQuote: 0,
+    trailingComma: 0,
+    semiColon: 0
+  },
+}
